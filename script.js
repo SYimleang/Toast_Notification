6d0395d9c1d84f94df092cb968126616ababd259
syimleang <syimleang@myseneca.ca>
2024-04-30 13:40:37 -0400

    Change to switch statement for future usage and add error handling

--- script.js
+++ script.js
@@ -4,19 +4,28 @@ const errorMsg = '<i class="fa-solid fa-circle-xmark"></i> Please fix the error!
 const invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i> Invalid input, please check again!';
 
 const showToast = (msg) => {
+    try {
     const toast = document.createElement('div');
     toast.classList.add('toast');
     toast.innerHTML = msg;
     toastBox.appendChild(toast);
 
-    if(msg.includes('error')) {
-        toast.classList.add('error');
-    }
-    else if (msg.includes('Invalid')) {
-        toast.classList.add('invalid');
+    switch(true) {
+
+        case msg.includes('error') :
+            toast.classList.add('error');
+            break;
+        case (msg.includes('Invalid')) :
+            toast.classList.add('invalid');
+            break;
+        default :
+            break;
     }
 
     setTimeout(() => {
         toast.remove();
     }, 6000);
+    } catch (error) {
+        console.error('Error occurred while showing toast: ', error);
+    }
 }
